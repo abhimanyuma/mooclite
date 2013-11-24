138f4cabe472fb1faf3c2fd47ab26a315665026e
Abhimanyu M A <abhimanyu.m.a@gmail.com>
2013-11-25 00:30:49 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mooclite::Application.config.secret_key_base = '3773c4b5bc02f35cc98757e1b1bf918225cf8a6c8bb9d316a3e219bc07916f625f7ffb8046aeb9f03048fc224e9c05d75244fe48b149217c643e9eb2b93f1404'
