42bf1cbe08e7e2284f193d1ae760ddbac472464d
Abhimanyu M A <abhimanyu@hackerrank.com>
2014-10-28 06:45:07 +0530

    Fix MongoDB bugs

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,6 @@ class Course
   include Mongoid::Timestamps
   include Mongoid::Paranoia
 
-  field :_id, type: BigDecimal, default: ->{name.parameterize}
 
   field :id, type: BigDecimal
   field :name, type: String
@@ -15,11 +14,20 @@ class Course
   field :created_at, type: Date
   field :updated_at, type: Date
 
+  field :_id, type: String, default: ->{ name.to_s.parameterize }
+
   validates_presence_of :name, :presence => true
+  validates_uniqueness_of :_id
   validates_presence_of :offered_by ,:unless => proc {new_record?}
   validates_presence_of :bio ,:unless => proc {new_record?}
   validates :bio, :length => {maximum:140}
 
+  before_validation(on: :create) do
+    self._id = self.name.parameterize
+  end
 
   has_many :lectures, dependent: :destroy
+
+  private
+
 end
